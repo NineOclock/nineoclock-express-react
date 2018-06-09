6d12c291353d0fb77c24ef28e80fd2b950e8c94b
apJammanbo <apJammanbo@gmail.com>
2018-06-09 18:53:46 +0900

    1차 Book 예제 완료

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@
 
 const express = require('express');
 const logger = require('./logger');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const {graphiqlExpress, graphqlExpress } = require('apollo-server-express');
+const { makeExecutableSchema } = require('graphql-tools');
 
 const argv = require('./argv');
 const port = require('./port');
@@ -11,13 +15,75 @@ const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngr
 const resolve = require('path').resolve;
 const app = express();
 
-// If you need a backend, e.g. an API, add your custom backend-specific middleware here
-// app.use('/api', myApi);
+// Add Backend(GraphQL)
+mongoose.connect('mongodb://jammanbo:jammanbo18@ds253840.mlab.com:53840/worklife').then(
+    () => console.log('connect')
+)
+
+const datas = [
+    {title: '진창규바보', author: '김준수'},
+    {title: '진창규개바보', author: '진창규'},
+    {title: '진창규핵바보', author: '최민식'},
+    {title: '진창규10바보', author: '이창민'},
+    {title: '진창규완전바보', author: '김수진'},
+]
+
+
+const Book = mongoose.model("book", {
+    _id: 'String',
+    title:'String',
+    author:'String'
+});
+
+// datas.forEach((book) => {
+//     new Book(book).save();
+// })
+
+const typeDefs = `
+    type Query {
+        books: [Book]
+    }
+    type Book {
+        id: String
+        title: String
+        author: String
+    }
+`;
+
+const resolvers = {
+    Query: {
+        books: (parent, args, context) => {
+            return context.book.find();
+        }
+    },
+    Book: {
+        id(parent, args) {
+            return parent._id;
+        },
+        title(parent, args) {
+            return parent.title;
+        },
+        author(parent, args) {
+            return parent.author;
+        }
+    }
+};
+
+
+const schema = makeExecutableSchema({
+    typeDefs,
+    resolvers,
+});
+
+
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: {book: Book} }));
+app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
-  publicPath: '/',
+    outputPath: resolve(process.cwd(), 'build'),
+    publicPath: '/',
 });
 
 // get the intended host and port number, use localhost and port 3000 if not provided
@@ -27,20 +93,20 @@ const prettyHost = customHost || 'localhost';
 
 // Start your app.
 app.listen(port, host, (err) => {
-  if (err) {
-    return logger.error(err.message);
-  }
+    if (err) {
+        return logger.error(err.message);
+    }
 
-  // Connect to ngrok in dev mode
-  if (ngrok) {
-    ngrok.connect(port, (innerErr, url) => {
-      if (innerErr) {
-        return logger.error(innerErr);
-      }
+    // Connect to ngrok in dev mode
+    if (ngrok) {
+        ngrok.connect(port, (innerErr, url) => {
+            if (innerErr) {
+                return logger.error(innerErr);
+            }
 
-      logger.appStarted(port, prettyHost, url);
-    });
-  } else {
-    logger.appStarted(port, prettyHost);
-  }
+            logger.appStarted(port, prettyHost, url);
+        });
+    } else {
+        logger.appStarted(port, prettyHost);
+    }
 });
